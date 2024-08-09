6e1fd7d3c01e03ba62d687c854895a013b5531b3
Cardanoian <superiorz1@yu.ac.kr>
2024-08-09 23:02:32 +0900

    Basic Express Server with typescript

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+import express, { Request, Response, Express } from 'express';
+
+const app: Express = express();
+const port: number = 18888;
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('<h1>Hello World!!!!!!!!</h1>');
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}.`);
+});
